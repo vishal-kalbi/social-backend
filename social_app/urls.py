f192e02bcbc6cf8a04aa2ac5c34b6f31944c70af
vishalzillion <vishal.zillioninfotech@gmail.com>
2024-08-31 18:13:18 +0530

    changed vercel configuration

--- social_app/urls.py
+++ social_app/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include  # Import include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import BasicAuthentication
+
+schema_view = get_schema_view(
+    title="API Documentation",
+    permission_classes=[AllowAny],  # Disable authentication for this view
+    authentication_classes=[],  # Remove authentication for schema view
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include the URLs from the 'social' app
+    path('api/', include('api.urls')),
+    path('', schema_view, name='schema'),
+    path('docs/', include_docs_urls(title='API Documentation')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- social_app/wsgi.py
+++ social_app/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_app.settings')
+
 application = get_wsgi_application()
 app = application

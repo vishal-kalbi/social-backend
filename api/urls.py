d6af8b9878f3840467e41143615be59445c7529e
vishalzillion <vishal.zillioninfotech@gmail.com>
2024-08-31 23:37:09 +0530

    changed urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from api.views import RegisterView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

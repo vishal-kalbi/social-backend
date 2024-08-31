9375f3101a686690504598ff17a82623352f3d40
vishalzillion <vishal.zillioninfotech@gmail.com>
2024-08-31 23:31:48 +0530

    added static

--- social_app/urls.py
+++ social_app/urls.py
@@ -4,6 +4,9 @@ from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 from rest_framework.permissions import AllowAny
 from rest_framework.authentication import BasicAuthentication
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # import logging
 
 # Set up the logger
@@ -24,3 +27,7 @@ urlpatterns = [
     path('', schema_view, name='schema'),
     # path('docs/', include_docs_urls(title='API Documentation')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

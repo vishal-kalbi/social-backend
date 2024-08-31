8f681eff7880864df90360154deae5273b191122
vishalzillion <vishal.zillioninfotech@gmail.com>
2024-08-31 18:37:46 +0530

    changes

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
 echo "BUILD END"
